ca9eb895fa0672c44e630cd96ec39f8f0a47015e
topisani <tobiaspisani@gmail.com>
2016-11-13 16:54:57 +0100

    suport for strings kinda

--- src/constants.rs
+++ src/constants.rs
@@ -1,8 +1,10 @@
 use std::fmt;
+use std::str;
 use num::FromPrimitive;
 
-#[derive(Debug)]
+#[derive(Debug, Default, Clone)]
 pub struct FuncConst {
+    pub name: &'static str,
     pub arg_count: u32,
     pub local_count: u32,
     pub body: Vec<u8>
@@ -26,6 +28,7 @@ pub fn read_const_table(data: &[u8]) -> ConstTable {
 
     let mut const_table: ConstTable = Default::default();
 
+    // TODO: Refactor these macros
     macro_rules! get_next_4_bytes {
         () => {{
             let val = (data[const_table.bc_counter] as u32) << 24 |
@@ -35,6 +38,20 @@ pub fn read_const_table(data: &[u8]) -> ConstTable {
             const_table.bc_counter += 4;
             debug!("get_next_4_bytes: 0x{:04X}", val);
             val
+        }}
+    }
+
+    // Eats a null byte terminated string
+    // TODO: Velo, take a look at this one, I'm sure it can be improved
+    macro_rules! eat_string {
+        () => {{
+            let mut str_len = const_table.bc_counter;
+            while data[str_len] != 0x00 {
+                str_len += 1;
+            }
+            let string = str::from_utf8(&data[const_table.bc_counter..str_len]);
+            const_table.bc_counter = str_len + 1; // + 1 for null byte
+            string
         }}
     }
 
@@ -51,15 +68,21 @@ pub fn read_const_table(data: &[u8]) -> ConstTable {
 
         match instr {
             ConstInstr::FUNC => {
-                // TODO: remove this on the compiler end
-                get_next_4_bytes!();
+                // TODO: use this id
+                let id = get_next_4_bytes!() as usize;
+                let name = eat_string!();
                 let arg_count = get_next_4_bytes!();
                 let local_count = get_next_4_bytes!();
                 let bc_length = get_next_4_bytes!() as usize;
+                let body = data[const_table.bc_counter..{const_table.bc_counter += bc_length; const_table.bc_counter + 1}].to_vec();
+                if id >= const_table.funcs.len() {
+                    const_table.funcs.resize(id + 1, FuncConst{..Default::default()})
+                }
                 const_table.funcs.push(FuncConst {
+                    name: name.unwrap(),
                     arg_count: arg_count,
                     local_count: local_count,
-                    body: data[const_table.bc_counter..{const_table.bc_counter += bc_length; const_table.bc_counter + 1}].to_vec()
+                    body: body
                 });
                 debug!("Added a function to the constants table");
             },
